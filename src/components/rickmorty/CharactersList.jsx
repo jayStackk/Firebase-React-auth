af23880170e045724ca609f3599dde167f772aa6
jayStackk <jmagpantay@codestack.co>
2023-08-11 13:36:56 -0700

    pushForNow, made a lot of progress so far.

--- src/components/rickmorty/CharactersList.jsx
+++ src/components/rickmorty/CharactersList.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
+import './CharactersList.css'
 
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredCharacters, setFilteredCharacters] = useState([]);
 
+
+  // this useEffect fetches my rick and morty data! 
+  // 
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character')
       .then(response => response.json())
@@ -25,8 +29,8 @@ const CharacterList = () => {
   };
 
   return (
-    <div md={12}>
-      <h2>Rick and Morty Characters</h2>
+    <div style={{ textAlign: 'center'}} md={12}>
+      {/* <h2 >Rick and Morty Characters</h2>
       <input
       style={{ backgroundColor: 'black'}}
       className='col-12'
@@ -35,7 +39,7 @@ const CharacterList = () => {
         value={searchTerm}
         onChange={e => setSearchTerm(e.target.value)}
       >
-        </input>
+        </input> */}
       <Button onClick={handleSearch}>Search</Button>
       {filteredCharacters.map(character => (
         <div key={character.id}>
@@ -43,7 +47,32 @@ const CharacterList = () => {
           <h5>{character.name}</h5>
         </div>
       ))}
+
+      <Row>
+        <Col>
+        <h2 >Rick and Morty Characters</h2>
+
+        </Col>
+
+        <Col>
+      <input
+      style={{ backgroundColor: 'pink', textAlign:'center',}}
+      className='col-12 morty'
+        type="text"
+        placeholder="Search character..."
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      >
+        </input>
+        
+        </Col>
+        
+        </Row>      
+
+
     </div>
+
+   
   );
 };
 
